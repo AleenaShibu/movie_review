fc67e5d697e42476d90514b8318cd120e023f7c1
Aleena Shibu <aleenashibu94@gmail.com>
2019-09-21 15:32:33 +0530

    created atemplate files made changes in project level settings

--- movie_review/settings.py
+++ movie_review/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c4@6@qzgatdr3-_j2d&s7719!sa478^&z9idaq82!9tq-vt2)s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ratings.apps.RatingsConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'movie_review.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
